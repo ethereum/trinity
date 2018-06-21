9c5411d3819c4de248492b2b920bcde83b415154
Piper Merriam <pipermerriam@gmail.com>
2018-06-21 11:34:31 -0600

    initial commit

---
+++ docs/js/terminal.js
@@ -0,0 +1,57 @@
+$(function() {
+  var data = [
+  { 
+    action: 'type',
+    //clear: true,
+    strings: ['trinity^400'],
+    output: $('.mimik-run-output').html(),
+    postDelay: 1
+  },
+  {
+    action: 'clear_prompt'
+  }
+  
+];
+  runScripts(data, 0);
+});
+
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.history').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 30,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'clear_prompt':
+          $('.cursors').remove();
+          break;
+    }
+}
