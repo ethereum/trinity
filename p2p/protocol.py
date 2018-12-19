5073ef817d8fc7f03539e78e88ad7a138a3ea71a
Jannik Luhn <jannik.luhn@brainbot.com>
2018-12-19 15:15:08 +0100

    Add TypedDict to allowed p2p message payload types

--- p2p/protocol.py
+++ p2p/protocol.py
@@ -13,6 +13,10 @@ from typing import (
     Union,
 )
 
+from mypy_extensions import (
+    TypedDict,
+)
+
 import snappy
 
 import rlp
@@ -30,10 +34,12 @@ from p2p._utils import get_devp2p_cmd_id
 if TYPE_CHECKING:
     from p2p.peer import BasePeer  # noqa: F401
 
+TypedDictPayloadType = TypedDict("TypedDictPayloadType", {})
 PayloadType = Union[
     Dict[str, Any],
     List[rlp.Serializable],
     Tuple[rlp.Serializable, ...],
+    TypedDictPayloadType,
 ]
 
 # A payload to be delivered with a request
--- trinity/protocol/bcc/proto.py
+++ trinity/protocol/bcc/proto.py
@@ -5,8 +5,11 @@ from eth2.beacon.types.attestations import Attestation
 
 from trinity.protocol.bcc.commands import (
     Status,
+    StatusMessage,
     GetBeaconBlocks,
+    GetBeaconBlocksMessage,
     BeaconBlocks,
+    BeaconBlocksMessage,
     AttestationRecords,
 )
 
@@ -35,13 +38,13 @@ class BCCProtocol(HasExtendedDebugLogger, Protocol):
 
     peer: "BCCPeer"
 
-    def send_handshake(self, genesis_hash: Hash32, best_hash: Hash32) -> None:
-        resp = {
-            "protocol_version": self.version,
-            "network_id": self.peer.network_id,
-            "genesis_hash": genesis_hash,
-            "best_hash": best_hash,
-        }
+    def send_handshake(self, genesis_hash: Hash32, head_slot: int) -> None:
+        resp = StatusMessage(
+            protocol_version=self.version,
+            network_id=self.peer.network_id,
+            genesis_hash=genesis_hash,
+            head_slot=head_slot,
+        )
         cmd = Status(self.cmd_id_offset, self.snappy_support)
         self.logger.debug2("Sending BCC/Status msg: %s", resp)
         self.send(*cmd.encode(resp))
@@ -50,16 +53,18 @@ class BCCProtocol(HasExtendedDebugLogger, Protocol):
                         block_slot_or_root: Union[BlockNumber, Hash32],
                         max_blocks: int) -> None:
         cmd = GetBeaconBlocks(self.cmd_id_offset, self.snappy_support)
-        data = {
-            'block_slot_or_root': block_slot_or_root,
-            'max_blocks': max_blocks,
-        }
-        header, body = cmd.encode(data)
+        header, body = cmd.encode(GetBeaconBlocksMessage(
+            block_slot_or_root=block_slot_or_root,
+            max_blocks=max_blocks,
+        ))
         self.send(header, body)
 
     def send_blocks(self, blocks: Tuple[BaseBeaconBlock, ...]) -> None:
         cmd = BeaconBlocks(self.cmd_id_offset, self.snappy_support)
-        header, body = cmd.encode(blocks)
+        header, body = cmd.encode(BeaconBlocksMessage(
+            request_id=request_id,
+            blocks=blocks,
+        ))
         self.send(header, body)
 
     def send_attestation_records(self, attestations: Tuple[Attestation, ...]) -> None:
