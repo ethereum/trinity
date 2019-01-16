71502c6329b7fdd2df02be78cb9cf0b8af163800
Jannik Luhn <jannik@brainbot.com>
2019-01-16 16:33:28 +0100

    Improve command test readability

--- tests/core/p2p-proto/bcc/test_commands.py
+++ tests/core/p2p-proto/bcc/test_commands.py
@@ -26,8 +26,7 @@ from .helpers import (
 )
 
 
-@pytest.mark.asyncio
-async def test_send_no_blocks(request, event_loop):
+async def get_command_setup(request, event_loop):
     alice, bob = await get_directly_linked_peers(
         request,
         event_loop,
@@ -37,27 +36,29 @@ async def test_send_no_blocks(request, event_loop):
     msg_buffer = MsgBuffer()
     bob.add_subscriber(msg_buffer)
 
-    alice.sub_proto.send_blocks((), request_id=5)
+    return alice, msg_buffer
+
+
+@pytest.mark.asyncio
+async def test_send_no_blocks(request, event_loop):
+    alice, msg_buffer = await get_command_setup(request, event_loop)
+
+    request_id = 5
+    alice.sub_proto.send_blocks((), request_id=request_id)
 
     message = await msg_buffer.msg_queue.get()
     assert isinstance(message.command, BeaconBlocks)
     assert message.payload == {
-        "request_id": 5,
+        "request_id": request_id,
         "blocks": (),
     }
 
 
 @pytest.mark.asyncio
 async def test_send_single_block(request, event_loop):
-    alice, bob = await get_directly_linked_peers(
-        request,
-        event_loop,
-        alice_chain_db=get_genesis_chain_db(),
-        bob_chain_db=get_genesis_chain_db(),
-    )
-    msg_buffer = MsgBuffer()
-    bob.add_subscriber(msg_buffer)
+    alice, msg_buffer = await get_command_setup(request, event_loop)
 
+    request_id = 5
     block = BeaconBlock(
         slot=1,
         parent_root=ZERO_HASH32,
@@ -67,27 +68,21 @@ async def test_send_single_block(request, event_loop):
         signature=(0, 0),
         body=empty_body(),
     )
-    alice.sub_proto.send_blocks((block,), request_id=5)
+    alice.sub_proto.send_blocks((block,), request_id=request_id)
 
     message = await msg_buffer.msg_queue.get()
     assert isinstance(message.command, BeaconBlocks)
     assert message.payload == {
-        "request_id": 5,
+        "request_id": request_id,
         "blocks": (block,),
     }
 
 
 @pytest.mark.asyncio
 async def test_send_multiple_blocks(request, event_loop):
-    alice, bob = await get_directly_linked_peers(
-        request,
-        event_loop,
-        alice_chain_db=get_genesis_chain_db(),
-        bob_chain_db=get_genesis_chain_db(),
-    )
-    msg_buffer = MsgBuffer()
-    bob.add_subscriber(msg_buffer)
+    alice, msg_buffer = await get_command_setup(request, event_loop)
 
+    request_id = 5
     blocks = tuple(
         BeaconBlock(
             slot=slot,
@@ -100,33 +95,27 @@ async def test_send_multiple_blocks(request, event_loop):
         )
         for slot in range(3)
     )
-    alice.sub_proto.send_blocks(blocks, request_id=5)
+    alice.sub_proto.send_blocks(blocks, request_id=request_id)
 
     message = await msg_buffer.msg_queue.get()
     assert isinstance(message.command, BeaconBlocks)
     assert message.payload == {
-        "request_id": 5,
+        "request_id": request_id,
         "blocks": blocks,
     }
 
 
 @pytest.mark.asyncio
 async def test_send_get_blocks_by_slot(request, event_loop):
-    alice, bob = await get_directly_linked_peers(
-        request,
-        event_loop,
-        alice_chain_db=get_genesis_chain_db(),
-        bob_chain_db=get_genesis_chain_db(),
-    )
-    msg_buffer = MsgBuffer()
-    bob.add_subscriber(msg_buffer)
+    alice, msg_buffer = await get_command_setup(request, event_loop)
 
-    alice.sub_proto.send_get_blocks(123, 10, request_id=5)
+    request_id = 5
+    alice.sub_proto.send_get_blocks(123, 10, request_id=request_id)
 
     message = await msg_buffer.msg_queue.get()
     assert isinstance(message.command, GetBeaconBlocks)
     assert message.payload == {
-        "request_id": 5,
+        "request_id": request_id,
         "block_slot_or_root": 123,
         "max_blocks": 10,
     }
@@ -134,21 +123,15 @@ async def test_send_get_blocks_by_slot(request, event_loop):
 
 @pytest.mark.asyncio
 async def test_send_get_blocks_by_hash(request, event_loop):
-    alice, bob = await get_directly_linked_peers(
-        request,
-        event_loop,
-        alice_chain_db=get_genesis_chain_db(),
-        bob_chain_db=get_genesis_chain_db(),
-    )
-    msg_buffer = MsgBuffer()
-    bob.add_subscriber(msg_buffer)
+    alice, msg_buffer = await get_command_setup(request, event_loop)
 
-    alice.sub_proto.send_get_blocks(b"\x33" * 32, 15, request_id=5)
+    request_id = 5
+    alice.sub_proto.send_get_blocks(b"\x33" * 32, 15, request_id=request_id)
 
     message = await msg_buffer.msg_queue.get()
     assert isinstance(message.command, GetBeaconBlocks)
     assert message.payload == {
-        "request_id": 5,
+        "request_id": request_id,
         "block_slot_or_root": b"\x33" * 32,
         "max_blocks": 15,
     }
@@ -156,14 +139,7 @@ async def test_send_get_blocks_by_hash(request, event_loop):
 
 @pytest.mark.asyncio
 async def test_send_no_attestations(request, event_loop):
-    alice, bob = await get_directly_linked_peers(
-        request,
-        event_loop,
-        alice_chain_db=get_genesis_chain_db(),
-        bob_chain_db=get_genesis_chain_db(),
-    )
-    msg_buffer = MsgBuffer()
-    bob.add_subscriber(msg_buffer)
+    alice, msg_buffer = await get_command_setup(request, event_loop)
 
     alice.sub_proto.send_attestation_records(())
 
@@ -174,14 +150,7 @@ async def test_send_no_attestations(request, event_loop):
 
 @pytest.mark.asyncio
 async def test_send_single_attestation(request, event_loop):
-    alice, bob = await get_directly_linked_peers(
-        request,
-        event_loop,
-        alice_chain_db=get_genesis_chain_db(),
-        bob_chain_db=get_genesis_chain_db(),
-    )
-    msg_buffer = MsgBuffer()
-    bob.add_subscriber(msg_buffer)
+    alice, msg_buffer = await get_command_setup(request, event_loop)
 
     attestation = Attestation(
         data=AttestationData(
@@ -207,20 +176,13 @@ async def test_send_single_attestation(request, event_loop):
 
 @pytest.mark.asyncio
 async def test_send_multiple_attestations(request, event_loop):
-    alice, bob = await get_directly_linked_peers(
-        request,
-        event_loop,
-        alice_chain_db=get_genesis_chain_db(),
-        bob_chain_db=get_genesis_chain_db(),
-    )
-    msg_buffer = MsgBuffer()
-    bob.add_subscriber(msg_buffer)
+    alice, msg_buffer = await get_command_setup(request, event_loop)
 
     attestations = tuple(
         Attestation(
             data=AttestationData(
                 slot=0,
-                shard=1,
+                shard=shard,
                 beacon_block_root=ZERO_HASH32,
                 epoch_boundary_root=ZERO_HASH32,
                 shard_block_root=ZERO_HASH32,
