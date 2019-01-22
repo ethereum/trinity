ethereum/py-evm

Add RANDAO validation

eth/beacon/state_machines/forks/serenity/operations.py
@@ -2,10 +2,35 @@ from eth.beacon.types.blocks import BaseBeaconBlock
 from eth.beacon.types.pending_attestation_records import PendingAttestationRecord
 from eth.beacon.types.states import BeaconState
 from eth.beacon.state_machines.configs import BeaconConfig
+from eth.beacon.helpers import get_beacon_proposer_index
+from eth._utils.numeric import bitwise_xor
 
 from .validation import (
     validate_serenity_attestation,
+    validate_serenity_randao_reveal,
 )
+
+
+def validate_randao(state: BeaconState,
+                    block: BaseBeaconBlock,
+                    config: BeaconConfig) -> BeaconState:
+    """
+    Implements 'per-block-processing.RANDAO' portion of Phase 0 spec:
+    https://github.com/ethereum/eth2.0-specs/blob/master/specs/core/0_beacon-chain.md#randao
+
+    Verify the ``randao_reveal`` contained within the ``block`` in the context of
+    ``randao_commitment`` in the ``state``
+
+    """
+    proposer = state.validator_registry[get_beacon_proposer_index(state,
+                                                                  state.slot,
+                                                                  config.EPOCH_LENGTH)]
+    validate_serenity_randao_reveal(block, proposer)
+    latest_randao_mix = state.latest_randao_mixes[state.slot % config.LATEST_RANDAO_MIXES_LENGTH]
+    latest_randao_mix = bitwise_xor(latest_randao_mix, block.randao_reveal)
+    proposer.randao_commitment = block.randao_reveal
+    proposer.randao_layers = 0
+    return state
 
 
 def process_attestations(state: BeaconState,

eth/beacon/state_machines/forks/serenity/validation.py
@@ -14,6 +14,7 @@ from eth.constants import (
 from eth._utils import bls as bls
 from eth.beacon._utils.hash import (
     hash_eth2,
+    repeat_hash_eth2,
 )
 
 from eth.beacon.enums import (
@@ -27,6 +28,8 @@ from eth.beacon.helpers import (
 from eth.beacon.types.states import BeaconState  # noqa: F401
 from eth.beacon.types.attestations import Attestation  # noqa: F401
 from eth.beacon.types.attestation_data import AttestationData  # noqa: F401
+from eth.beacon.types.blocks import BaseBeaconBlock
+from eth.beacon.types.validator_records import ValidatorRecord
 
 
 #
@@ -246,4 +249,21 @@ def validate_serenity_attestation_aggregate_signature(state: Type[BeaconState],
     if not is_valid_signature:
         raise ValidationError(
             "Attestation ``aggregate_signature`` is invalid."
+        )
+
+
+#
+# RANDAO reveal
+#
+
+def validate_serenity_randao_reveal(block: BaseBeaconBlock,
+                                    proposer: ValidatorRecord) -> None:
+    """
+    Validate ``randao_reveal`` field of ``block``.
+    Raise ``ValidationError`` if it's invalid.
+    """
+    if repeat_hash_eth2(block.randao_reveal, proposer.randao_layers) != proposer.randao_commitment:
+        raise ValidationError(
+            "``randao_reveal`` in the block does not match with "
+            "``randao_commitment`` of the proposer."
         )

ethereum/trinity

eth2/beacon/state_machines/forks/serenity/operations.py
@@ -2,10 +2,35 @@ from eth2.beacon.types.blocks import BaseBeaconBlock
 from eth2.beacon.types.pending_attestation_records import PendingAttestationRecord
 from eth2.beacon.types.states import BeaconState
 from eth2.beacon.state_machines.configs import BeaconConfig
+from eth2.beacon.helpers import get_beacon_proposer_index
+from eth2._utils.numeric import bitwise_xor
 
 from .validation import (
     validate_serenity_attestation,
+    validate_serenity_randao_reveal,
 )
+
+
+def validate_randao(state: BeaconState,
+                    block: BaseBeaconBlock,
+                    config: BeaconConfig) -> BeaconState:
+    """
+    Implements 'per-block-processing.RANDAO' portion of Phase 0 spec:
+    https://github.com/ethereum/eth2.0-specs/blob/master/specs/core/0_beacon-chain.md#randao
+
+    Verify the ``randao_reveal`` contained within the ``block`` in the context of
+    ``randao_commitment`` in the ``state``
+
+    """
+    proposer = state.validator_registry[get_beacon_proposer_index(state,
+                                                                  state.slot,
+                                                                  config.EPOCH_LENGTH)]
+    validate_serenity_randao_reveal(block, proposer)
+    latest_randao_mix = state.latest_randao_mixes[state.slot % config.LATEST_RANDAO_MIXES_LENGTH]
+    latest_randao_mix = bitwise_xor(latest_randao_mix, block.randao_reveal)
+    proposer.randao_commitment = block.randao_reveal
+    proposer.randao_layers = 0
+    return state
 
 
 def process_attestations(state: BeaconState,

eth2/beacon/state_machines/forks/serenity/validation.py
@@ -13,6 +13,7 @@ from eth.constants import (
 from eth2._utils import bls as bls
 from eth2.beacon._utils.hash import (
     hash_eth2,
+    repeat_hash_eth2,
 )
 
 from eth2.beacon.enums import (
@@ -29,6 +30,7 @@ from eth2.beacon.types.states import BeaconState  # noqa: F401
 from eth2.beacon.types.attestations import Attestation  # noqa: F401
 from eth2.beacon.types.attestation_data import AttestationData  # noqa: F401
 from eth2.beacon.types.proposal_signed_data import ProposalSignedData
+from eth2.beacon.types.validator_records import ValidatorRecord
 from eth2.beacon.typing import (
     ShardNumber,
 )
@@ -283,4 +285,21 @@ def validate_serenity_attestation_aggregate_signature(state: BeaconState,
     if not is_valid_signature:
         raise ValidationError(
             "Attestation ``aggregate_signature`` is invalid."
+        )
+
+
+#
+# RANDAO reveal
+#
+
+def validate_serenity_randao_reveal(block: BaseBeaconBlock,
+                                    proposer: ValidatorRecord) -> None:
+    """
+    Validate ``randao_reveal`` field of ``block``.
+    Raise ``ValidationError`` if it's invalid.
+    """
+    if repeat_hash_eth2(block.randao_reveal, proposer.randao_layers) != proposer.randao_commitment:
+        raise ValidationError(
+            "``randao_reveal`` in the block does not match with "
+            "``randao_commitment`` of the proposer."
         )