dcd17be5529b19ca2a644685292704494215bbe2
mhchia <kevin.mh.chia@gmail.com>
2019-03-29 16:25:06 +0800

    Organize the code a bit
    Add typing and make validator interact with the slot ticker

--- trinity/plugins/eth2/beacon/plugin.py
+++ trinity/plugins/eth2/beacon/plugin.py
@@ -1,13 +1,3 @@
-from trinity.config import BeaconChainConfig
-from eth2.beacon.types.blocks import (
-    BeaconBlock,
-)
-from eth2.beacon.tools.builder.proposer import (
-    _get_proposer_index,
-    create_block_on_state,
-)
-from eth2.beacon.chains.base import BeaconChain
-from eth_keys.datatypes import PrivateKey
 import asyncio
 from argparse import (
     ArgumentParser,
@@ -17,12 +7,29 @@ from typing import (
     NewType,
 )
 
+from eth_keys.datatypes import PrivateKey
+
+
+from eth2.beacon.chains.base import BeaconChain
+from eth2.beacon.state_machines.forks.serenity.blocks import (
+    SerenityBeaconBlock,
+)
+from eth2.beacon.state_machines.base import BaseBeaconStateMachine  # noqa: F401
+from eth2.beacon.tools.builder.proposer import (
+    _get_proposer_index,
+    create_block_on_state,
+)
+from eth2.beacon.types.blocks import BaseBeaconBlock
+from eth2.beacon.types.states import BeaconState
 from p2p import ecies
 from p2p.constants import DEFAULT_MAX_PEERS
 from trinity._utils.shutdown import (
     exit_with_service_and_endpoint,
 )
-from trinity.config import BeaconAppConfig
+from trinity.config import (
+    BeaconAppConfig,
+    BeaconChainConfig,
+)
 from trinity.endpoint import TrinityEventBusEndpoint
 from trinity.extensibility import BaseIsolatedPlugin
 from trinity.protocol.bcc.peer import BCCPeerPool
@@ -35,37 +42,6 @@ from trinity.db.beacon.manager import (
 from trinity.plugins.eth2.beacon.testing_blocks_generators import (
     get_ten_blocks_context,
 )
-
-from eth2.beacon.state_machines.forks.serenity.blocks import (
-    SerenityBeaconBlock,
-)
-
-
-async def _get_current_slot(genesis_time, seconds_per_slot):
-    import time
-    now = int(time.time())
-    return max((now - genesis_time) // seconds_per_slot, 0)
-
-
-class SlotTicker:
-    def __init__(self, genesis_time, seconds_per_slot, validator, state_machine):
-        self._genesis_time = genesis_time
-        self._seconds_per_slot = seconds_per_slot
-        self._validator = validator
-        self._state_machine = state_machine
-        self._task = asyncio.ensure_future(self._job())
-        self.latest_slot = 0
-
-    async def _job(self):
-        await asyncio.sleep(self._seconds_per_slot)
-        slot = await self._get_current_slot(self._genesis_time, self._seconds_per_slot)
-        if slot > self.latest_slot:
-            self.latest_slot = slot
-            self._validator.new_slot(slot)
-            self._state_machine.new_slot(slot)
-
-    def cancel(self):
-        self._task.cancel()
 
 
 class BeaconNodePlugin(BaseIsolatedPlugin):
@@ -131,11 +107,18 @@ class BeaconNodePlugin(BaseIsolatedPlugin):
             server.cancel_token,
         )
 
+        validator = Validator(
+            beacon_config=beacon_config,
+            chain=chain,
+            peer_pool=server.peer_pool,
+            privkey=privkey,
+        )
+
         slot_ticker = SlotTicker(
             genesis_time=chain_config.genesis_time,
             seconds_per_slot=6,
             validator=validator,
-            state_machine=state_machine,
+            state_machine=chain.get_state_machine(at_block=chain.get_canonical_head()),
         )
 
         loop = asyncio.get_event_loop()
@@ -155,9 +138,6 @@ class BeaconNodePlugin(BaseIsolatedPlugin):
         loop.close()
 
 
-SlotTicker = NewType('SlotTicker', object)
-
-
 class Validator:
     """
     Reference: https://github.com/ethereum/trinity/blob/master/eth2/beacon/tools/builder/proposer.py#L175  # noqa: E501
@@ -166,44 +146,95 @@ class Validator:
     chain: BeaconChain
     peer_pool: BCCPeerPool
     privkey: PrivateKey
-    slot_ticker: SlotTicker
 
     def __init__(
             self,
             beacon_config: BeaconChainConfig,
             chain: BeaconChain,
             peer_pool: BCCPeerPool,
-            privkey: PrivateKey,
-            slot_ticker: SlotTicker) -> None:
+            privkey: PrivateKey) -> None:
         self.beacon_config = beacon_config
         self.chain = chain
         self.peer_pool = peer_pool
         self.privkey = privkey
-        self.slot_ticker = slot_ticker
 
-    def propose_block(self):
-        block = self._make_proposing_block()
+    def new_slot(self, slot: int) -> None:
+        """
+        The callback for `SlotTicker`, to be called whenever new slot is ticked.
+        """
+        self.propose_block(slot=slot)
+
+    def propose_block(self, slot: int) -> None:
+        block = self._make_proposing_block(slot)
         # TODO: broadcast the block to the peers in `self.peer_pool`
 
-    def _make_proposing_block(self) -> BeaconBlock:
-        parent_block = self.chain.get_canonical_head()
-        slot = self.slot_ticker.next_slot()
-        state_machine = self.chain.get_state_machine()
+    def _get_caononical_head(self) -> BaseBeaconBlock:
+        return self.chain.get_canonical_head()
+
+    def _get_head_state_machine(self) -> BaseBeaconStateMachine:
+        return self.chain.get_state_machine(at_block=self._get_caononical_head())
+
+    def _get_head_state(self) -> BeaconState:
+        return self._get_head_state_machine().state
+
+    def _make_proposing_block(self, slot: int) -> BaseBeaconBlock:
+        parent_block = self._get_caononical_head()
+        state_machine = self._get_head_state_machine()
+        state = self._get_head_state()
         proposer_index = _get_proposer_index(
             state_machine,
-            self.chain.state,
+            state,
             slot,
             parent_block.root,
             self.beacon_config,
         )
         return create_block_on_state(
-            state=self.chain.state,
+            state=state,
             config=self.beacon_config,
             state_machine=state_machine,
             block_class=self.chain.get_block_class(),
             parent_block=parent_block,
-            slot=self.slot_ticker.next_slot(),
+            slot=slot,
             validator_index=proposer_index,
             privkey=self.privkey,
             attestations=[],
         )
+
+
+async def _get_current_slot(genesis_time, seconds_per_slot):
+    import time
+    now = int(time.time())
+    return max((now - genesis_time) // seconds_per_slot, 0)
+
+
+class SlotTicker:
+    _genesis_time: int
+    _seconds_per_slot: int
+    _validator: Validator
+    _state_machine: BaseBeaconStateMachine
+    _task: asyncio.Task
+    latest_slot: int
+
+    def __init__(
+            self,
+            genesis_time: int,
+            seconds_per_slot: int,
+            validator: Validator,
+            state_machine: BaseBeaconStateMachine) -> None:
+        self._genesis_time = genesis_time
+        self._seconds_per_slot = seconds_per_slot
+        self._validator = validator
+        self._state_machine = state_machine
+        self._task = asyncio.ensure_future(self._job())
+        self.latest_slot = 0
+
+    async def _job(self) -> None:
+        await asyncio.sleep(self._seconds_per_slot)
+        slot = await _get_current_slot(self._genesis_time, self._seconds_per_slot)
+        if slot > self.latest_slot:
+            self.latest_slot = slot
+            self._validator.new_slot(slot)
+            # self._state_machine.new_slot(slot)
+
+    def cancel(self) -> None:
+        self._task.cancel()
