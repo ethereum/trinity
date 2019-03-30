70b7e4d6ab518bbca5c383862c67e9c80fbd5391
mhchia <kevin.mh.chia@gmail.com>
2019-03-30 20:19:06 +0800

    Generate genesis.json when running the script

--- eth2/beacon/scripts/gen_genesis_json.py
+++ eth2/beacon/scripts/gen_genesis_json.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 import json
+import sys
 import time
+import os.path
 
+
+file_genesis_json = f"{os.path.dirname(sys.argv[0])}/genesis.json"
 genesis_time = int(time.time())
 genesis_json = {
     "genesis_time": genesis_time
 }
-with open("genesis.json", "w") as f:
+with open(file_genesis_json, "w") as f:
     json.dump(genesis_json, f)
--- eth2/beacon/scripts/run_beacon_nodes.sh
+++ eth2/beacon/scripts/run_beacon_nodes.sh
@@ -6,6 +6,7 @@
 DIR_ROOT="/tmp/ttt"
 DIR_ALICE="$DIR_ROOT/alice"
 DIR_BOB="$DIR_ROOT/bob"
+EXE_GEN_GENESIS="$(dirname $0)/gen_genesis_json.py"
 PORT_ALICE=30304
 PORT_BOB=30305
 INDEX_ALICE=0
@@ -16,6 +17,9 @@ TIME_BOB_AFTER_ALICE=30
 CMD_ALICE="trinity-beacon --mock-blocks=true --validator_index=$INDEX_ALICE --port=$PORT_ALICE --trinity-root-dir=$DIR_ALICE --beacon-nodekey=6b94ffa2d9b8ee85afb9d7153c463ea22789d3bbc5d961cc4f63a41676883c19 -l debug"
 CMD_BOB="trinity-beacon --validator_index=$INDEX_BOB --port=$PORT_BOB --trinity-root-dir=$DIR_BOB --port=5566 --beacon-nodekey=f5ad1c57b5a489fc8f21ad0e5a19c1f1a60b8ab357a2100ff7e75f3fa8a4fd2e --bootstrap_nodes=enode://c289557985d885a3f13830a475d649df434099066fbdc840aafac23144f6ecb70d7cc16c186467f273ad7b29707aa15e6a50ec3fde35ae2e69b07b3ddc7a36c7@0.0.0.0:30303  -l debug"
 
+# generate the genesis.json
+
+$EXE_GEN_GENESIS
 
 rm -rf $DIR_ALICE $DIR_BOB >/dev/null
 mkdir -p $DIR_ALICE $DIR_BOB >/dev/null
@@ -23,7 +27,6 @@ mkdir -p $DIR_ALICE $DIR_BOB >/dev/null
 echo "Spinning up ALICE"
 $CMD_ALICE &
 PID_ALICE=$!
-echo $PID_ALICE
 PIDS_TO_KILL=$PID_ALICE
 
 trap "echo \"Killing nodes...\"; kill -9 $PIDS_TO_KILL; echo '\nDone\n'; exit;" SIGINT
