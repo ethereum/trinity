85f12645ed1806417612c98443889bbb6d4255db
mhchia <kevin.mh.chia@gmail.com>
2019-03-30 22:00:58 +0800

    Refactor `gen_genesis_json.py`
    And change `genesis.json` path, to trinity_root_dir for each node

--- trinity/plugins/eth2/beacon/gen_genesis_json.py
+++ trinity/plugins/eth2/beacon/gen_genesis_json.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 
-import json
-import sys
 import time
-import os.path
 
 
 # NOTE: Should be `trinity/genesis.json`. Because our .py is under trinity/eth2/beacon/scripts,
 #   `genesis.json` is in three layers above.
-file_genesis_json = f"{os.path.dirname(sys.argv[0])}/../../../genesis.json"
 genesis_time = int(time.time())
 genesis_json = {
     "genesis_time": genesis_time
 }
-with open(file_genesis_json, "w") as f:
-    json.dump(genesis_json, f)
+print(genesis_json)
