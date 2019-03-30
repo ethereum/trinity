f9da38b1856fb51dbf95c6a426786e3dfa3d7700
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-03-30 15:13:28 +0800

    Rename BeaconConfig to Eth2Config and move it to `eth2.configs`

---
+++ tests/eth2/bls_stub.py
@@ -0,0 +1,10 @@
+def bls_verify(pubkey, message_hash, signature, domain):
+    return True
+
+
+def bls_verify_multiple(pubkeys, message_hashes, signature, domain):
+    return True
+
+
+def bls_aggregate_pubkeys(pubkeys):
+    return b'\x42' * 96
