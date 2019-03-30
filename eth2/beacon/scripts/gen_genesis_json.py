f87dfe91b398142d0892c00c55d5a4674b91f1df
mhchia <kevin.mh.chia@gmail.com>
2019-03-30 21:09:47 +0800

    Fix wrong port in the script
    Besides, generate `genesis.json` under trinity root

--- eth2/beacon/scripts/gen_genesis_json.py
+++ eth2/beacon/scripts/gen_genesis_json.py
@@ -6,7 +6,9 @@ import time
 import os.path
 
 
-file_genesis_json = f"{os.path.dirname(sys.argv[0])}/genesis.json"
+# NOTE: Should be `trinity/genesis.json`. Because our .py is under trinity/eth2/beacon/scripts,
+#   `genesis.json` is in three layers above.
+file_genesis_json = f"{os.path.dirname(sys.argv[0])}/../../../genesis.json"
 genesis_time = int(time.time())
 genesis_json = {
     "genesis_time": genesis_time
