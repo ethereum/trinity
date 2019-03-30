454c5c0019c5c1e723c72996545cc7fd99e9d226
mhchia <kevin.mh.chia@gmail.com>
2019-03-30 22:47:51 +0800

    Remove unneeded `DIR_MAINNET`

--- eth2/beacon/scripts/run_beacon_nodes.sh
+++ eth2/beacon/scripts/run_beacon_nodes.sh
@@ -12,11 +12,10 @@ TIME_BOB_AFTER_ALICE=30
 
 CMD_ALICE="trinity-beacon --validator_index=$INDEX_ALICE --port=$PORT_ALICE --trinity-root-dir=$DIR_ALICE --beacon-nodekey=6b94ffa2d9b8ee85afb9d7153c463ea22789d3bbc5d961cc4f63a41676883c19 --mock-blocks=true -l debug"
 CMD_BOB="trinity-beacon --validator_index=$INDEX_BOB --port=$PORT_BOB --trinity-root-dir=$DIR_BOB --beacon-nodekey=f5ad1c57b5a489fc8f21ad0e5a19c1f1a60b8ab357a2100ff7e75f3fa8a4fd2e --bootstrap_nodes=enode://c289557985d885a3f13830a475d649df434099066fbdc840aafac23144f6ecb70d7cc16c186467f273ad7b29707aa15e6a50ec3fde35ae2e69b07b3ddc7a36c7@0.0.0.0:$PORT_ALICE -l debug"
-DIR_MAINNET="mainnet"
 FILE_GENESIS_JSON="genesis.json"
 
 rm -rf $DIR_ALICE $DIR_BOB >/dev/null
-mkdir -p $DIR_ALICE/$DIR_MAINNET $DIR_BOB/$DIR_MAINNET >/dev/null
+mkdir -p $DIR_ALICE $DIR_BOB >/dev/null
 
 # generate the genesis.json
 $EXE_GEN_GENESIS > "$DIR_ALICE/$FILE_GENESIS_JSON"
