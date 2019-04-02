4dce028f0b72d65575f90ac06c994afb555f6c85
mhchia <kevin.mh.chia@gmail.com>
2019-04-02 19:32:08 +0800

    Ensure logs are emitted
    - "Running server"
    - "their head slot"

--- eth2/beacon/scripts/run_beacon_nodes.py
+++ eth2/beacon/scripts/run_beacon_nodes.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python
 
 import asyncio
+from collections import (
+    defaultdict,
+)
+import logging
 import os.path
 import signal
 import sys
+import time
+from typing import (
+    Dict,
+    List,
+    MutableSet,
+    NamedTuple,
+)
 
 
 dir_root = "/tmp/ttt"
 dir_alice = f"{dir_root}/alice"
 dir_bob = f"{dir_root}/bob"
-exe_gen_genesis = f"{os.path.dirname(sys.argv[0])}/../../../trinity/plugins/eth2/beacon/gen_genesis_json.py"
+exe_gen_genesis = f"{os.path.dirname(sys.argv[0])}/../../../trinity/plugins/eth2/beacon/gen_genesis_json.py"  # noqa: E501
 port_alice = 30304
 port_bob = 30305
 index_alice = 0
 index_bob = 1
-cmd_alice = f"trinity-beacon --validator_index={index_alice} --port={port_alice} --trinity-root-dir={dir_alice} --beacon-nodekey=6b94ffa2d9b8ee85afb9d7153c463ea22789d3bbc5d961cc4f63a41676883c19 --mock-blocks=true -l debug"
-cmd_bob = f"trinity-beacon --validator_index={index_bob} --port={port_bob} --trinity-root-dir={dir_bob} --beacon-nodekey=f5ad1c57b5a489fc8f21ad0e5a19c1f1a60b8ab357a2100ff7e75f3fa8a4fd2e --bootstrap_nodes=enode://c289557985d885a3f13830a475d649df434099066fbdc840aafac23144f6ecb70d7cc16c186467f273ad7b29707aa15e6a50ec3fde35ae2e69b07b3ddc7a36c7@0.0.0.0:{port_alice} -l debug"
+cmd_alice = f"trinity-beacon --validator_index={index_alice} --port={port_alice} --trinity-root-dir={dir_alice} --beacon-nodekey=6b94ffa2d9b8ee85afb9d7153c463ea22789d3bbc5d961cc4f63a41676883c19 --mock-blocks=true -l debug"  # noqa: E501
+cmd_bob = f"trinity-beacon --validator_index={index_bob} --port={port_bob} --trinity-root-dir={dir_bob} --beacon-nodekey=f5ad1c57b5a489fc8f21ad0e5a19c1f1a60b8ab357a2100ff7e75f3fa8a4fd2e --bootstrap_nodes=enode://c289557985d885a3f13830a475d649df434099066fbdc840aafac23144f6ecb70d7cc16c186467f273ad7b29707aa15e6a50ec3fde35ae2e69b07b3ddc7a36c7@0.0.0.0:{port_alice} -l debug"  # noqa: E501
 file_genesis_json = "genesis.json"
 
 time_bob_wait_for_alice = 30
-
-
-async def read_log(name, stream_reader):
-    while True:
-        line = (await stream_reader.readline()).decode('utf-8').replace('\n', '')
-        print(f"{name}: {line}")
-        await asyncio.sleep(0.01)
 
 
 async def run(cmd):
@@ -35,6 +39,109 @@ async def run(cmd):
         stderr=asyncio.subprocess.PIPE,
     )
     return proc
+
+
+class NodeEvent(NamedTuple):
+    name: str
+    pattern: str
+    # TODO: dependent event?
+    timeout: int
+
+
+class EventTimeOutError(Exception):
+    pass
+
+
+SERVER_RUNNING = NodeEvent(name="server running", pattern="Running server", timeout=60)
+START_SYNCING = NodeEvent(name="start syncing", pattern="their head slot", timeout=200)
+
+
+nodes_to_stop = []
+
+
+def stop_all_nodes():
+    global nodes_to_stop
+    for node in nodes_to_stop:
+        print(f"Stopping node={node}")
+        node.stop()
+
+
+class Node:
+    name: str
+    cmd: str
+    start_time: float
+    proc: asyncio.subprocess.Process
+    # TODO: use CancelToken instead
+    tasks: List[asyncio.Task]
+    events_expected: Dict[str, MutableSet[NodeEvent]]
+    has_event_happend: Dict[NodeEvent, bool]
+
+    logger = logging.getLogger("eth2.beacon.scripts.run_beacon_nodes.Node")
+
+    def __init__(self, name: str, cmd: str) -> None:
+        self.name = name
+        self.cmd = cmd
+        self.tasks = []
+        self.start_time = time.monotonic()
+        self.events_expected = {}
+        self.events_expected["stdout"] = set()
+        self.events_expected["stderr"] = set([SERVER_RUNNING])
+        self.has_event_happend = defaultdict(lambda: False)
+        asyncio.ensure_future(self._run(self.cmd))
+
+    def __repr__(self) -> str:
+        return f"<Node {self.name} {self.proc}"
+
+    def stop(self) -> None:
+        for task in self.tasks:
+            task.cancel()
+        self.proc.terminate()
+
+    def add_event(self, from_stream: str, event: NodeEvent) -> None:
+        if from_stream not in ("stdout", "stderr"):
+            return
+        self.events_expected[from_stream].add(event)
+
+    async def _run(self, cmd: str) -> None:
+        print(f"Spinning up {self.name}")
+        self.proc = await run(cmd)
+        self.tasks.append(asyncio.ensure_future(self._print_logs('stdout', self.proc.stdout)))
+        self.tasks.append(asyncio.ensure_future(self._print_logs('stderr', self.proc.stderr)))
+        try:
+            await self._event_monitor()
+        except EventTimeOutError as e:
+            print(e)
+            self.logger.debug(e)
+            # FIXME: nasty
+            stop_all_nodes()
+            sys.exit(2)
+
+    async def _event_monitor(self):
+        while True:
+            for from_stream, events in self.events_expected.items():
+                for event in events:
+                    current_time = time.monotonic()
+                    ellapsed_time = current_time - self.start_time
+                    if not self.has_event_happend[event] and (ellapsed_time > event.timeout):
+                        raise EventTimeOutError(
+                            f"Event {event.name!r} is time out, "
+                            f"which should have occurred in {from_stream}."
+                        )
+            await asyncio.sleep(0.1)
+
+    async def _print_logs(self, from_stream: str, stream_reader: asyncio.StreamReader) -> None:
+        async for line_bytes in stream_reader:
+            line = line_bytes.decode('utf-8').replace('\n', '')
+            # TODO: Preprocessing
+            self._record_happenning_event(from_stream, line)
+            print(f"{self.name}.{from_stream}: {line}")
+
+
+    def _record_happenning_event(self, from_stream, line):
+        for event in self.events_expected[from_stream]:
+            if event.pattern in line:
+                self.logger.debug(f"event \"event.name\" occurred in {from_stream}")
+                self.has_event_happend[event] = True
 
 
 async def main():
@@ -56,30 +163,21 @@ async def main():
     )
     await proc.wait()
 
-    procs_to_stop = []
-
     def sigint_handler(sig, frame):
-        for proc in procs_to_stop:
-            print(f"Stopping proc={proc}")
-            proc.terminate()
+        stop_all_nodes()
         sys.exit(123)
 
     signal.signal(signal.SIGINT, sigint_handler)
 
-    print("Spinning up Alice")
-    proc_alice = await run(cmd_alice)
-    procs_to_stop.append(proc_alice)
-    asyncio.ensure_future(read_log("Alice.stdout", proc_alice.stdout))
-    asyncio.ensure_future(read_log("Alice.stderr", proc_alice.stderr))
+    node_alice = Node('Alice', cmd_alice)
+    nodes_to_stop.append(node_alice)
 
     print(f"Sleeping {time_bob_wait_for_alice} seconds to wait until Alice is initialized")
     await asyncio.sleep(time_bob_wait_for_alice)
 
-    print("Spinning up Bob")
-    proc_bob = await run(cmd_bob)
-    procs_to_stop.append(proc_bob)
-    asyncio.ensure_future(read_log("Bob.stdout", proc_bob.stdout))
-    asyncio.ensure_future(read_log("Bob.stderr", proc_bob.stderr))
+    node_bob = Node('Bob', cmd_bob)
+    node_bob.add_event("stderr", START_SYNCING)
+    nodes_to_stop.append(node_bob)
 
     await asyncio.sleep(1000000)
 
