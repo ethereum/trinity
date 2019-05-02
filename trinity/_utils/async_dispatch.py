052b45a05263e6f18a87c8fe09351c761b2fd37b
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2019-05-02 18:37:14 +0200

    Allow `async_method` to resolve nested methods

--- trinity/_utils/async_dispatch.py
+++ trinity/_utils/async_dispatch.py
@@ -1,5 +1,6 @@
 import asyncio
 import functools
+import operator
 from typing import (
     Any,
     Awaitable,
@@ -10,8 +11,7 @@ from typing import (
 def async_method(method_name: str) -> Callable[..., Any]:
     async def method(self: Any, *args: Any, **kwargs: Any) -> Awaitable[Any]:
         loop = asyncio.get_event_loop()
-
-        func = getattr(self, method_name)
+        func = operator.attrgetter(method_name)(self)
         pfunc = functools.partial(func, *args, **kwargs)
 
         return await loop.run_in_executor(None, pfunc)
