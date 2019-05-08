0ea0b5dee8663977be01733c3d91979273690fa1
Brian Cloutier <github@briancloutier.com>
2019-05-07 19:33:33 -0700

    Defer plugin creation until they can be initialized

--- trinity/plugins/eth2/network_generator/plugin.py
+++ trinity/plugins/eth2/network_generator/plugin.py
@@ -77,7 +77,8 @@ class NetworkGeneratorPlugin(BaseMainProcessPlugin):
     def name(self) -> str:
         return "NetworkGenerator"
 
-    def configure_parser(self, arg_parser: ArgumentParser, subparser: _SubParsersAction) -> None:
+    @classmethod
+    def configure_parser(cls, arg_parser: ArgumentParser, subparser: _SubParsersAction) -> None:
 
         testnet_generator_parser = subparser.add_parser(
             'testnet',
@@ -102,78 +103,87 @@ class NetworkGeneratorPlugin(BaseMainProcessPlugin):
             default=60,
         )
 
-        testnet_generator_parser.set_defaults(func=self.run_generate_testnet_dir)
+        testnet_generator_parser.set_defaults(func=cls.run_generate_testnet_dir)
 
-    def run_generate_testnet_dir(self, args: Namespace, trinity_config: TrinityConfig) -> None:
+    @classmethod
+    def run_generate_testnet_dir(cls, args: Namespace, trinity_config: TrinityConfig) -> None:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self._run_generate_testnet_dir(args))
+        loop.run_until_complete(cls._run_generate_testnet_dir(args))
         loop.close()
 
-    async def _run_generate_testnet_dir(self, args: Namespace) -> None:
-        self.logger.info("Generating testnet")
-        self.network_dir = args.network_dir
-        if len(os.listdir(self.network_dir)) > 0:
-            self.logger.error("This directory is not empty, won't create network files here.")
+    @classmethod
+    async def _run_generate_testnet_dir(cls, args: Namespace) -> None:
+        logger = cls.get_logger()
+        logger.info("Generating testnet")
+        cls.network_dir = args.network_dir
+        if len(os.listdir(cls.network_dir)) > 0:
+            logger.error("This directory is not empty, won't create network files here.")
             sys.exit(1)
 
-        self.generate_trinity_root_dirs()
-        self.generate_keys(args.num)
-        self.generate_genesis_state(args.genesis_delay)
+        cls.generate_trinity_root_dirs()
+        cls.generate_keys(args.num)
+        cls.generate_genesis_state(args.genesis_delay)
 
-        self.logger.info(bold_green("Network generation completed"))
+        logger.info(bold_green("Network generation completed"))
 
-    def generate_keys(self, num: int) -> None:
-        self.logger.info(f"Creating {num} validators' keys")
-        self.keys_dir = self.network_dir / KEYS_DIR
-        self.keys_dir.mkdir()
+    @classmethod
+    def generate_keys(cls, num: int) -> None:
+        logger = cls.get_logger()
+        logger.info(f"Creating {num} validators' keys")
+        cls.keys_dir = cls.network_dir / KEYS_DIR
+        cls.keys_dir.mkdir()
 
         privkeys = tuple(int.from_bytes(
             hash_eth2(str(i).encode('utf-8'))[:4], 'big')
             for i in range(num)
         )
-        self.keymap = {bls.privtopub(key): key for key in privkeys}
+        cls.keymap = {bls.privtopub(key): key for key in privkeys}
 
-        num_of_clients = len(self.clients)
+        num_of_clients = len(cls.clients)
         for validator_index, key in enumerate(privkeys):
             file_name = f"v{validator_index:07d}.privkey"
-            private_key_path = self.keys_dir / file_name
+            private_key_path = cls.keys_dir / file_name
             with open(private_key_path, "w") as f:
                 f.write(str(key))
 
             # Distribute keys to clients
-            client = self.clients[validator_index % num_of_clients]
+            client = cls.clients[validator_index % num_of_clients]
             with open(client.validator_keys_dir / file_name, "w") as f:
                 f.write(str(key))
 
-    def generate_genesis_state(self, genesis_delay: Second) -> None:
+    @classmethod
+    def generate_genesis_state(cls, genesis_delay: Second) -> None:
+        logger = cls.get_logger()
         state_machine_class = XiaoLongBaoStateMachine
 
         # Since create_mock_genesis takes a long time, update the real genesis_time later
         dummy_time = Timestamp(int(time.time()))
         state, _ = create_mock_genesis(
-            num_validators=len(self.keymap.keys()),
+            num_validators=len(cls.keymap.keys()),
             config=state_machine_class.config,
-            keymap=self.keymap,
+            keymap=cls.keymap,
             genesis_block_class=state_machine_class.block_class,
             genesis_time=dummy_time,
         )
-        self.logger.info(f"Genesis time will be {genesis_delay} seconds from now")
+        logger.info(f"Genesis time will be {genesis_delay} seconds from now")
         genesis_time = Timestamp(int(time.time()) + genesis_delay)
         state = state.copy(
             genesis_time=genesis_time,
         )
         yaml = YAML()
-        with open(self.network_dir / GENESIS_FILE, "w") as f:
+        with open(cls.network_dir / GENESIS_FILE, "w") as f:
             yaml.dump(to_formatted_dict(state), f)
 
         # Distribute genesis file to clients
-        for client in self.clients:
+        for client in cls.clients:
             with open(client.client_dir / GENESIS_FILE, "w") as f:
                 yaml.dump(to_formatted_dict(state), f)
 
-    def generate_trinity_root_dirs(self) -> None:
-        self.logger.info("Generating root directories for clients")
-        self.clients = tuple(Client(name, self.network_dir) for name in ("alice", "bob"))
-        for client in self.clients:
+    @classmethod
+    def generate_trinity_root_dirs(cls) -> None:
+        logger = cls.get_logger()
+        logger.info("Generating root directories for clients")
+        cls.clients = tuple(Client(name, cls.network_dir) for name in ("alice", "bob"))
+        for client in cls.clients:
             client.client_dir.mkdir()
             client.validator_keys_dir.mkdir()
