06d122d0d747e17c311330b6962127d85ecf0a6d
Chih Cheng Liang <chihchengliang@gmail.com>
2019-05-22 13:29:02 +0800

    improve str(AttestationData): two spaces for group name. previous root for crosslink

--- eth2/beacon/types/attestation_data.py
+++ eth2/beacon/types/attestation_data.py
@@ -59,9 +59,9 @@ class AttestationData(ssz.Serializable):
 
     def __str__(self) -> str:
         return (
-            f"LMD slot={self.slot} root={humanize_hash(self.beacon_block_root)} | "
-            f"FFG epoch={self.source_epoch} "
+            f"LMD  slot={self.slot} root={humanize_hash(self.beacon_block_root)} | "
+            f"FFG  epoch={self.source_epoch} "
             f"{humanize_hash(self.source_root)}<-{humanize_hash(self.target_root)} | "
-            f"CL shard={self.shard} previous={str(self.previous_crosslink)} "
-            f"data_root={humanize_hash(self.crosslink_data_root)}"
+            f"CL  shard={self.shard} {humanize_hash(self.previous_crosslink.root)}"
+            f"<-{humanize_hash(self.crosslink_data_root)}"
         )
