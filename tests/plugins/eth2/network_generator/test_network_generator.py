aa85730a54e70c53dfba53980fc572802c6df608
Chih Cheng Liang <chihchengliang@gmail.com>
2019-06-01 22:10:11 +0800

    test_missing_genesis_time_arg

--- tests/plugins/eth2/network_generator/test_network_generator.py
+++ tests/plugins/eth2/network_generator/test_network_generator.py
@@ -20,3 +20,17 @@ async def test_directory_generation(async_process_runner, command, tmpdir):
     assert await contains_all(async_process_runner.stderr, {
         "Network generation completed",
     })
+
+
+@pytest.mark.parametrize(
+    'command',
+    (
+        ('trinity-beacon', 'testnet', "--num=1",),
+    )
+)
+@pytest.mark.asyncio
+async def test_missing_genesis_time_arg(async_process_runner, command):
+    await async_process_runner.run(command, timeout_sec=30)
+    assert await contains_all(async_process_runner.stderr, {
+        "one of the arguments --genesis-delay --genesis-time is required",
+    })
