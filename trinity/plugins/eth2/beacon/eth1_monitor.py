ccf9d499287c63700ec5c09de3744d85a0826786
mhchia <kevin.mh.chia@gmail.com>
2019-06-10 09:21:01 -0400

    Add subscribe function in Eth1Monitor service

--- trinity/plugins/eth2/beacon/eth1_monitor.py
+++ trinity/plugins/eth2/beacon/eth1_monitor.py
@@ -1,4 +1,5 @@
 from typing import (
+    Callable,
     NamedTuple,
 )
 
@@ -13,16 +14,27 @@ from trinity._utils.shellart import (
 )
 
 
-class DepositContractData(NamedTuple):
+class DepositContractInfo(NamedTuple):
     contract_address: bytes
     event_abi_deposit: str
     event_abi_eth2genesis: str
 
 
+class GenesisLogData(NamedTuple):
+    pass
+
+
+class DepositLogData(NamedTuple):
+    pass
+
 
 # DEPOSIT_CONTRACT_ADDRESS = 0x1234
 # # https://github.com/ethereum/deposit_contract/blob/dev/deposit_contract/contracts/validator_registration.v.py#L12-L19  # noqa: E501
 # DEPOSIT_EVENT_ABI = ""
+
+
+CBGenesisData = Callable[[GenesisLogData], None]
+CBDepositData = Callable[[DepositLogData], None]
 
 
 class Eth1Monitor(BaseService):
@@ -31,15 +43,25 @@ class Eth1Monitor(BaseService):
     - Callback when
     """
     _web3: web3.Web3
-    _deposit_contract_data: DepositContractData
+    _deposit_contract_data: DepositContractInfo
     _event_filter: web3.eth.filter
+    # callback which is called when a new genesis log is emmited
+    _cb_genesis_data: CBGenesisData
+    # callback which is called when a new deposit log is emmited
+    _cb_deposit_data: CBDepositData
 
-    def __init__(self, _web3: web3.Web3, _deposit_contract_data: DepositContractData) -> None:
+    def __init__(self, _web3: web3.Web3, _deposit_contract_data: DepositContractInfo) -> None:
         self._web3 = _web3
         self._deposit_contract_data = _deposit_contract_data
 
-    def register_event_filter(self):
+    def register_event_filter(self) -> None:
         pass
+
+    def subscribe_genesis_data(self, cb_func: CBGenesisData) -> None:
+        self._cb_genesis_data = cb_func
+
+    def subscribe_deposit_data(self, cb_func: CBDepositData) -> None:
+        self._cb_deposit_data = cb_func
 
     async def _run(self) -> None:
         await self.event_bus.wait_until_serving()
