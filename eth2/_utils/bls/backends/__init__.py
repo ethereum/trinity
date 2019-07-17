ef1b1779ca477bffd434717bc18437a48cdf0c9c
Chih Cheng Liang <chihchengliang@gmail.com>
2019-07-17 21:35:52 +0800

    fix typing bug

--- eth2/_utils/bls/backends/__init__.py
+++ eth2/_utils/bls/backends/__init__.py
@@ -1,19 +1,25 @@
 
 from .noop import NoOpBackend
 from .py_ecc import PyECCBackend
+from .base import BaseBLSBackend  # noqa: F401
+from typing import (  # noqa: F401
+    Type,
+    Tuple,
+)
 
 
 AVAILABLE_BACKENDS = (
     NoOpBackend,
     PyECCBackend,
-)
-
+)  # type: Tuple[Type[BaseBLSBackend], ...]
 
 # If blspy not installed, use PyECC as default BLS backend
+
+DEFAULT_BACKEND = None  # type: Type[BaseBLSBackend]
 
 try:
     from .chia import ChiaBackend
     DEFAULT_BACKEND = ChiaBackend
-    AVAILABLE_BACKENDS += ChiaBackend
+    AVAILABLE_BACKENDS += (ChiaBackend,)
 except ImportError:
     DEFAULT_BACKEND = PyECCBackend
