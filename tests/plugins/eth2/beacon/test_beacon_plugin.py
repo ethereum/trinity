32e1320f960e7b6bb6961eb73f1d9e66d7d51a32
Chih Cheng Liang <chihchengliang@gmail.com>
2019-07-19 17:12:01 +0800

    enable beacon plugin test

--- tests/plugins/eth2/beacon/test_beacon_plugin.py
+++ tests/plugins/eth2/beacon/test_beacon_plugin.py
@@ -1,11 +1,10 @@
 import pytest
+import asyncio
 from trinity._utils.async_iter import (
     contains_all
 )
 
 
-# FIXME: this test should not be skipped after genesis json is added
-@pytest.mark.skip(reason="need genesis json to initialize `genesis_data`")
 @pytest.mark.parametrize(
     'command',
     (
@@ -13,7 +12,16 @@ from trinity._utils.async_iter import (
     )
 )
 @pytest.mark.asyncio
-async def test_plugin_boot(async_process_runner, command):
+async def test_plugin_boot(async_process_runner, command, tmpdir):
+    testnet_path = tmpdir / "testnet"
+    testnet_path.mkdir()
+
+    proc = await asyncio.create_subprocess_shell(
+        f"trinity-beacon testnet --num=1 --genesis-delay=1 --network-dir={testnet_path}"
+    )
+    await proc.wait()
+
+    command = command + (f"--trinity-root-dir={testnet_path/'alice'}", )
     await async_process_runner.run(command, timeout_sec=30)
     assert await contains_all(async_process_runner.stderr, {
         "Running server",
