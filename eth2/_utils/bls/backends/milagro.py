86afb1924d2dd9416562ab95bdbbfd398be06d6e
Chih Cheng Liang <chihchengliang@gmail.com>
2019-08-21 18:25:16 +0800

    pass bls yaml tests

--- eth2/_utils/bls/backends/milagro.py
+++ eth2/_utils/bls/backends/milagro.py
@@ -31,8 +31,12 @@ from eth2.beacon.constants import (
 )
 
 
-def to_little(domain: Domain) -> int:
-    return int.from_bytes(domain, 'little')
+def to_int(domain: Domain) -> int:
+    """
+    Convert Domain to big endian int since
+    sigp/milagro_bls use big endian int on hash to g2.
+    """
+    return int.from_bytes(domain, 'big')
 
 
 class MilagroBackend(BaseBLSBackend):
@@ -44,7 +48,7 @@ class MilagroBackend(BaseBLSBackend):
     def sign(message_hash: Hash32,
              privkey: int,
              domain: Domain) -> BLSSignature:
-        return sign(message_hash, privkey.to_bytes(48, 'big'), to_little(domain))
+        return sign(message_hash, privkey.to_bytes(48, 'big'), to_int(domain))
 
     @staticmethod
     def verify(message_hash: Hash32,
@@ -53,21 +57,21 @@ class MilagroBackend(BaseBLSBackend):
                domain: Domain) -> bool:
         if pubkey == EMPTY_PUBKEY:
             raise ValueError(f"Empty public key breaks Milagro binding  pubkey={pubkey}")
-        return verify(message_hash, pubkey, signature, to_little(domain))
+        return verify(message_hash, pubkey, signature, to_int(domain))
 
     @staticmethod
     def aggregate_signatures(signatures: Sequence[BLSSignature]) -> BLSSignature:
         # py_ecc use a different EMPTY_SIGNATURE. Return the Trinity one here:
         if len(signatures) == 0:
             return EMPTY_SIGNATURE
-        return aggregate_signatures(signatures)
+        return aggregate_signatures(list(signatures))
 
     @staticmethod
     def aggregate_pubkeys(pubkeys: Sequence[BLSPubkey]) -> BLSPubkey:
         # py_ecc use a different EMPTY_PUBKEY. Return the Trinity one here:
         if len(pubkeys) == 0:
             return EMPTY_PUBKEY
-        return aggregate_pubkeys(pubkeys)
+        return aggregate_pubkeys(list(pubkeys))
 
     @staticmethod
     def verify_multiple(pubkeys: Sequence[BLSPubkey],
@@ -76,4 +80,4 @@ class MilagroBackend(BaseBLSBackend):
                         domain: Domain) -> bool:
         if signature == EMPTY_SIGNATURE:
             raise ValueError(f"Empty signature breaks Milagro binding  signature={signature}")
-        return verify_multiple(pubkeys, message_hashes, signature, to_little(domain))
+        return verify_multiple(list(pubkeys), list(message_hashes), signature, to_int(domain))
