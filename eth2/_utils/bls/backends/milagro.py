e907c58c6fd206f54a5199de7852101c7ebb63d9
Chih Cheng Liang <chihchengliang@gmail.com>
2019-08-21 21:30:44 +0800

    remove unhelpful comments

--- eth2/_utils/bls/backends/milagro.py
+++ eth2/_utils/bls/backends/milagro.py
@@ -71,7 +71,6 @@ class MilagroBackend(BaseBLSBackend):
 
     @staticmethod
     def aggregate_signatures(signatures: Sequence[BLSSignature]) -> BLSSignature:
-        # py_ecc use a different EMPTY_SIGNATURE. Return the Trinity one here:
         non_empty_signatures = tuple(sig for sig in signatures if sig != EMPTY_SIGNATURE)
         if len(non_empty_signatures) == 0:
             return EMPTY_SIGNATURE
@@ -79,7 +78,6 @@ class MilagroBackend(BaseBLSBackend):
 
     @staticmethod
     def aggregate_pubkeys(pubkeys: Sequence[BLSPubkey]) -> BLSPubkey:
-        # py_ecc use a different EMPTY_PUBKEY. Return the Trinity one here:
         non_empty_pubkeys = tuple(key for key in pubkeys if key != EMPTY_PUBKEY)
         if len(non_empty_pubkeys) == 0:
             return EMPTY_PUBKEY
