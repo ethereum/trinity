1a55c694ef7c56eb0adc9455c6e4083f62cac22f
Jannik Luhn <jannik@brainbot.com>
2019-09-05 13:11:49 +0200

    Add methods to get nodes by distance

--- p2p/discv5/routing_table.py
+++ p2p/discv5/routing_table.py
@@ -1,6 +1,9 @@
 from collections import (
     deque,
 )
+from functools import (
+    partial,
+)
 import logging
 import secrets
 from typing import (
@@ -11,6 +14,7 @@ from typing import (
 )
 
 from eth_utils import (
+    big_endian_to_int,
     encode_hex,
 )
 
@@ -19,11 +23,23 @@ from p2p.discv5.typing import (
 )
 
 
+def compute_distance(left_node_id: NodeID, right_node_id: NodeID) -> int:
+    left_int = big_endian_to_int(left_node_id)
+    right_int = big_endian_to_int(right_node_id)
+    return left_int ^ right_int
+
+
+def compute_log_distance(left_node_id: NodeID, right_node_id: NodeID) -> int:
+    distance = compute_distance(left_node_id, right_node_id)
+    return distance.bit_length()
+
+
 class FlatRoutingTable(Collection[NodeID]):
 
     logger = logging.getLogger("p2p.discv5.routing_table.FlatRoutingTable")
 
-    def __init__(self) -> None:
+    def __init__(self, local_node_id: NodeID) -> None:
+        self.local_node_id = local_node_id
         self.entries: Deque[NodeID] = deque()
 
     def add(self, node_id: NodeID) -> None:
@@ -60,7 +76,24 @@ class FlatRoutingTable(Collection[NodeID]):
         return len(self.entries)
 
     def __iter__(self) -> Iterator[NodeID]:
-        return iter(self.entries)
+        return self.iter_nodes_around(self.local_node_id)
+
+    def iter_nodes_around(self, center: NodeID) -> Iterator[NodeID]:
+        # very inefficient, but easy to understand and most likely not a bottleneck
+        node_ids = self.entries
+        compute_distance_to_center = partial(compute_distance, center)
+        return iter(sorted(node_ids, key=compute_distance_to_center))
+
+    def iter_nodes_at_log_distance(self, log_distance: int) -> Iterator[NodeID]:
+        compute_log_distance_to_center = partial(compute_log_distance, self.local_node_id)
+        for node_id in self:
+            log_distance_to_center = compute_log_distance_to_center(node_id)
+            if log_distance_to_center < log_distance:
+                continue
+            elif log_distance_to_center == log_distance:
+                yield node_id
+            elif log_distance_to_center > log_distance:
+                break
 
     def get_random_entry(self) -> NodeID:
         return secrets.choice(self.entries)
