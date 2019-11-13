976f80c2443681cc9f7281d967dafa5c637d7f30
Jason Carver <ut96caarrs@snkmail.com>
2019-11-13 14:02:50 -0800

    remove unused SnappyCommand

--- trinity/protocol/common/commands.py
+++
@@ -1,6 +0,0 @@
-from p2p.protocol import Command
-
-
-class SnappyCommand(Command):
-    def __init__(self, cmd_id_offset: int) -> None:
-        super().__init__(cmd_id_offset, snappy_support=True)
