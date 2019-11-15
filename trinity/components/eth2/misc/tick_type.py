2af8d538792f03100fa8ccf1203d5abbd8c1839e
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-11-15 15:52:50 +0800

    type hint

--- trinity/components/eth2/misc/tick_type.py
+++ trinity/components/eth2/misc/tick_type.py
@@ -8,13 +8,13 @@ class TickType(IntEnum):
     SLOT_TWO_THIRD = 2  # SECONDS_PER_SLOT * 2 / 3
 
     @property
-    def is_start(self):
+    def is_start(self) -> bool:
         return self == self.SLOT_START
 
     @property
-    def is_one_third(self):
+    def is_one_third(self) -> bool:
         return self == self.SLOT_ONE_THIRD
 
     @property
-    def is_two_third(self):
+    def is_two_third(self) -> bool:
         return self == self.SLOT_TWO_THIRD
