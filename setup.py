54a4d49eddbe18a3cb7a4af3d2813bb25216f7ed
Jannik Luhn <jannik@brainbot.com>
2019-12-11 14:44:24 +0700

    Update py-ssz version

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ deps = {
         "py-ecc==1.7.1",
         "rlp>=1.1.0,<2.0.0",
         PYEVM_DEPENDENCY,
-        "ssz==0.1.5",
+        "ssz==0.2.0",
     ],
     'eth2-extra': [
         "milagro-bls-binding==0.1.3",
