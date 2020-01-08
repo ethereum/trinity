9bff1772f882ceec338ed4bf7a24888000b46e97
Jannik Luhn <jannik@brainbot.com>
2020-01-08 18:58:35 +0100

    Implement file enr db

--- p2p/discv5/enr_db.py
+++ p2p/discv5/enr_db.py
@@ -1,7 +1,18 @@
+import binascii
+from collections import defaultdict
 import logging
+import operator
+import pathlib
+import re
+import time
 from typing import (
+    DefaultDict,
     Dict,
 )
+
+from eth_utils import ValidationError
+
+import rlp
 
 import trio
 
@@ -11,6 +22,9 @@ from p2p.discv5.abc import EnrDbApi
 from p2p.discv5.enr import ENR
 from p2p.discv5.identity_schemes import IdentitySchemeRegistry
 from p2p.discv5.typing import NodeID
+
+
+ACCEPTABLE_ENR_LOAD_TIME = 1.0
 
 
 class BaseEnrDb(EnrDbApi):
@@ -44,6 +58,138 @@ class BaseEnrDb(EnrDbApi):
             await self.update(enr)
         except KeyError:
             await self.insert(enr)
+
+
+def get_enr_filename(enr: ENR) -> str:
+    return f"enr_{encode_hex(enr.node_id)}_{enr.sequence_number}"
+
+
+def is_valid_enr_filename(filename: str) -> bool:
+    return bool(re.match(r"^enr_0x[0-9a-f]{64}_\d+$", filename))
+
+
+class FileEnrDb(BaseEnrDb):
+    def __init__(self,
+                 identity_scheme_registry: IdentitySchemeRegistry,
+                 directory: pathlib.Path) -> None:
+        super().__init__(identity_scheme_registry)
+        self.directory = directory
+        self.enrs: DefaultDict[NodeID, Dict[int, ENR]] = defaultdict(dict)
+        self.load_enrs_timed()
+
+    def load_enrs_timed(self) -> None:
+        start_time = time.time()
+
+        self.load_enrs()
+
+        end_time = time.time()
+        total_time = end_time - start_time
+
+        if total_time > ACCEPTABLE_ENR_LOAD_TIME:
+            self.logger.warning("Loading ENRs took a very long time: %.1f seconds", total_time)
+        else:
+            self.logger.debug("Loading ENRs took %.1f seconds", total_time)
+
+    def load_enrs(self) -> None:
+        enr_paths = tuple(
+            path for path in self.directory.iterdir()
+            if path.is_file()
+        )
+
+        invalid_enr_paths = tuple(
+            path for path in enr_paths
+            if not is_valid_enr_filename(path.name)
+        )
+        for invalid_enr_path in invalid_enr_paths:
+            self.logger.warning(
+                "Encountered invalid ENR filename %s in ENR directory %s",
+                invalid_enr_path,
+                self.directory
+            )
+
+        valid_enr_paths = tuple(
+            path for path in enr_paths
+            if path not in invalid_enr_paths
+        )
+        for path in valid_enr_paths:
+            try:
+                enr = self.load_enr_file(path)
+            except (binascii.Error, rlp.DeserializationError, ValidationError) as error:
+                self.logger.warning("Encountered invalid ENR in file %s: %s", path, error)
+                continue
+
+            if get_enr_filename(enr) != path.name:
+                self.logger.warning(
+                    "ENR in %s has inconsistent name (actual node id: %s, actual seq num: %d)",
+                    path,
+                    encode_hex(enr.node_id),
+                    enr.sequence_number
+                )
+
+            self.enrs[enr.node_id][enr.sequence_number] = enr
+
+    def load_enr_file(self, path: pathlib.Path) -> ENR:
+        enr_base64 = path.read_text()
+        enr = ENR.from_repr(enr_base64, self.identity_scheme_registry)
+        return enr
+
+    def write_enr_file(self, enr: ENR) -> None:
+        path = self.directory / get_enr_filename(enr)
+        path.write_text(repr(enr))
+
+    async def insert(self, enr: ENR) -> None:
+        self.validate_identity_scheme(enr)
+
+        if await self.contains(enr.node_id):
+            raise ValueError("ENR with node id %s already exists", encode_hex(enr.node_id))
+        else:
+            self.logger.debug(
+                "Inserting new ENR of %s with sequence number %d",
+                encode_hex(enr.node_id),
+                enr.sequence_number,
+            )
+            self.enrs[enr.node_id][enr.sequence_number] = enr
+            self.write_enr_file(enr)
+
+    async def update(self, enr: ENR) -> None:
+        self.validate_identity_scheme(enr)
+
+        existing_enr = await self.get(enr.node_id)
+        if existing_enr.sequence_number < enr.sequence_number:
+            self.logger.debug(
+                "Updating ENR of %s from sequence number %d to %d",
+                encode_hex(enr.node_id),
+                existing_enr.sequence_number,
+                enr.sequence_number,
+            )
+            self.enrs[enr.node_id][enr.sequence_number] = enr
+            self.write_enr_file(enr)
+        else:
+            self.logger.debug(
+                "Not updating ENR of %s as new sequence number %d is not higher than the current "
+                "one %d",
+                encode_hex(enr.node_id),
+                enr.sequence_number,
+                existing_enr.sequence_number,
+            )
+
+    async def get(self, node_id: NodeID) -> ENR:
+        await trio.hazmat.checkpoint()
+        enrs = self.enrs[node_id]
+        if not enrs:
+            raise KeyError(f"No ENR for node {encode_hex(node_id)} present in DB")
+        else:
+            # get enr with highest sequence number
+            _, enr = max(enrs.items(), key=operator.itemgetter(0))
+            return enr
+
+    async def contains(self, node_id: NodeID) -> bool:
+        await trio.hazmat.checkpoint()
+        return bool(self.enrs[node_id])
+
+    def __len__(self) -> int:
+        return len(self.enrs)
+
 
 class MemoryEnrDb(BaseEnrDb):
 
