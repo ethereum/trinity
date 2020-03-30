f6236ce3c67f002717d80a56e70b8e06dbdc6a3b
Alex Stokes <r.alex.stokes@gmail.com>
2020-03-30 13:32:17 -0700

    make an in-process trio component (marker type)

--- trinity/extensibility/trio.py
+++ trinity/extensibility/trio.py
@@ -13,8 +13,16 @@ from trinity._utils.mp import ctx
 from trinity._utils.profiling import profiler
 from trinity.boot_info import BootInfo
 
-from .component import BaseIsolatedComponent
+from .component import BaseIsolatedComponent, BaseComponent
 from .event_bus import TrioEventBusService
+
+
+class TrioComponent(BaseComponent):
+    """
+    ``TrioComponent`` is a component that executes in-process
+    with the ``trio`` concurrency library.
+    """
+    pass
 
 
 class TrioIsolatedComponent(BaseIsolatedComponent):
