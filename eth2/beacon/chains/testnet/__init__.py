8caf7b5982b7a72420e8fcb16cb863f10bf8cf9c
Alex Stokes <r.alex.stokes@gmail.com>
2020-03-30 13:31:51 -0700

    disable discv5 component

--- eth2/beacon/chains/testnet/__init__.py
+++ eth2/beacon/chains/testnet/__init__.py
@@ -6,3 +6,12 @@ class SkeletonLakeChain(BeaconChain):
     sm_configuration = (
         (SkeletonLakeStateMachine.config.GENESIS_SLOT, SkeletonLakeStateMachine),
     )
+
+
+class MinimalChain(SkeletonLakeChain):
+    """
+    ``MinimalChain`` is an alias for the ``BeaconChain`` using the
+    "minimal" configuration profile.
+    """
+
+    pass
