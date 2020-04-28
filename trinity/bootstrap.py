ff4a81ed09a0fb225aab8d0f2bc92f764a44ed61
Guilherme Salgado <gsalgado@gmail.com>
2020-04-28 11:56:06 +0800

    Use asyncio-run-in-process to run the DBManager

--- trinity/bootstrap.py
+++ trinity/bootstrap.py
@@ -9,16 +9,19 @@ import signal
 from typing import (
     Callable,
     Dict,
-    Iterable,
     Sequence,
     Tuple,
     Type,
 )
 
+from asyncio_run_in_process import open_in_process
 from async_service import AsyncioManager
-from eth_utils import ValidationError
-from eth_utils.toolz import curry
 
+from eth.db.backends.level import LevelDB
+
+from eth_utils import ValidationError
+
+from trinity.db.manager import DBManager
 from trinity.exceptions import (
     AmbigiousFileSystem,
     MissingPath,
@@ -45,10 +48,11 @@ from trinity.network_configurations import (
     PRECONFIGURED_NETWORKS,
 )
 from trinity._utils.ipc import (
-    kill_process_gracefully,
     remove_dangling_ipc_files,
+    wait_for_ipc,
 )
 from trinity._utils.logging import (
+    child_process_logging,
     enable_warnings_by_default,
     set_logger_levels,
     setup_file_logging,
@@ -219,22 +223,61 @@ def validate_component_cli(component_types: Tuple[Type[BaseComponentAPI], ...],
             parser.exit(message=str(exc))
 
 
-@curry
-def kill_trinity_with_reason(trinity_config: TrinityConfig,
-                             processes: Tuple[multiprocessing.Process, ...],
-                             reason: str) -> None:
-    logger = logging.getLogger()
-    kill_trinity_gracefully(
-        trinity_config,
-        logger,
-        processes,
-        reason=reason
-    )
+async def run_db_manager(
+        boot_info: BootInfo,
+        get_base_db_fn: Callable[[BootInfo], LevelDB]) -> None:
+    with child_process_logging(boot_info):
+        trinity_config = boot_info.trinity_config
+        manager = DBManager(get_base_db_fn(boot_info))
+        with trinity_config.process_id_file('database'):
+            with manager.run(trinity_config.database_ipc_path):
+                loop = asyncio.get_event_loop()
+                try:
+                    await loop.run_in_executor(None, manager.wait_stopped)
+                finally:
+                    # We always need to call stop() before returning as asyncio can't cancel the
+                    # thread started by run_in_executor() and that would prevent
+                    # open_in_process(run_db_manager, ...) from returning.
+                    manager.stop()
 
 
-def _run_asyncio_components(component_types: Tuple[Type[BaseComponentAPI], ...],
-                            boot_info: BootInfo,
-                            processes: Tuple[multiprocessing.Process, ...]) -> None:
+async def _run(
+        boot_info: BootInfo,
+        get_base_db_fn: Callable[[BootInfo], LevelDB],
+        component_manager: AsyncioManager) -> None:
+    logger = logging.getLogger('trinity')
+    start_new_session = True
+    if os.getenv('TRINITY_SINGLE_PROCESS_GROUP') == "1":
+        # This is needed because some of our integration tests rely on all processes being in
+        # a single process group.
+        start_new_session = False
+    async with open_in_process(
+            run_db_manager,
+            boot_info,
+            get_base_db_fn,
+            subprocess_kwargs={'start_new_session': start_new_session},
+    ) as db_proc:
+        logger.info("Started DB server process (pid=%d)", db_proc.pid)
+        try:
+            wait_for_ipc(boot_info.trinity_config.database_ipc_path)
+        except TimeoutError:
+            logger.error("Timeout waiting for database to start.  Exiting...")
+            argparse.ArgumentParser().error(message="Timed out waiting for database start")
+            return None
+
+        try:
+            await component_manager.run()
+        finally:
+            try:
+                await component_manager.stop()
+            finally:
+                logger.info("Terminating DB process")
+                db_proc.send_signal(signal.SIGINT)
+
+
+def run(component_types: Tuple[Type[BaseComponentAPI], ...],
+        boot_info: BootInfo,
+        get_base_db_fn: Callable[[BootInfo], LevelDB]) -> None:
     runtime_component_types = tuple(
         component_cls
         for component_cls in component_types
@@ -247,12 +290,12 @@ def _run_asyncio_components(component_types: Tuple[Type[BaseComponentAPI], ...],
         boot_info,
         runtime_component_types,
     )
-    manager = AsyncioManager(component_manager_service)
+    component_manager_manager = AsyncioManager(component_manager_service)
 
     loop = asyncio.get_event_loop()
     loop.add_signal_handler(
         signal.SIGTERM,
-        manager.cancel,
+        component_manager_manager.cancel,
         'SIGTERM',
     )
     loop.add_signal_handler(
@@ -261,14 +304,18 @@ def _run_asyncio_components(component_types: Tuple[Type[BaseComponentAPI], ...],
         'CTRL+C',
     )
 
+    logger = logging.getLogger()
     try:
-        loop.run_until_complete(manager.run())
-    except BaseException as err:
-        logger = logging.getLogger()
-        logger.error("Error during trinity run: %r", err)
+        loop.run_until_complete(_run(boot_info, get_base_db_fn, component_manager_manager))
+    except BaseException:
+        logger.exception("Error during trinity run")
         raise
     finally:
-        kill_trinity_with_reason(trinity_config, processes, component_manager_service.reason)
+        reason = component_manager_service.reason
+        hint = f" ({reason})" if reason else f""
+        logger.info('Shutting down Trinity%s', reason)
+        remove_dangling_ipc_files(logger, trinity_config.ipc_dir)
+        argparse.ArgumentParser().exit(message=f"Trinity shutdown complete{hint}\n")
         if trinity_config.trinity_tmp_root_dir:
             shutil.rmtree(trinity_config.trinity_root_dir)
 
@@ -315,6 +362,7 @@ def construct_boot_info(app_identifier: str,
 
 
 def main_entry(trinity_boot: BootFn,
+               get_base_db_fn: Callable[[BootInfo], LevelDB],
                app_identifier: str,
                component_types: Tuple[Type[BaseComponentAPI], ...],
                sub_configs: Sequence[Type[BaseAppConfig]]) -> None:
@@ -333,8 +381,8 @@ def main_entry(trinity_boot: BootFn,
 
     # Setup the log listener which child processes relay their logs through
     with IPCListener(*handlers).run(trinity_config.logging_ipc_path):
-        processes = trinity_boot(boot_info)
-        _run_asyncio_components(component_types, boot_info, processes)
+        trinity_boot(boot_info)
+        run(component_types, boot_info, get_base_db_fn)
 
 
 def display_launch_logs(trinity_config: TrinityConfig) -> None:
@@ -343,34 +391,3 @@ def display_launch_logs(trinity_config: TrinityConfig) -> None:
     logger.info("Started main process (pid=%d)", os.getpid())
     logger.info(construct_trinity_client_identifier())
     logger.info("Trinity DEBUG log file is created at %s", str(trinity_config.logfile_path))
-
-
-def kill_trinity_gracefully(trinity_config: TrinityConfig,
-                            logger: logging.Logger,
-                            processes: Iterable[multiprocessing.Process],
-                            reason: str = None) -> None:
-    # When a user hits Ctrl+C in the terminal, the SIGINT is sent to all processes in the
-    # foreground *process group*, so both our networking and database processes will terminate
-    # at the same time and not sequentially as we'd like. That shouldn't be a problem but if
-    # we keep getting unhandled BrokenPipeErrors/ConnectionResetErrors like reported in
-    # https://github.com/ethereum/py-evm/issues/827, we might want to change the networking
-    # process' signal handler to wait until the DB process has terminated before doing its
-    # thing.
-    # Notice that we still need the kill_process_gracefully() calls here, for when the user
-    # simply uses 'kill' to send a signal to the main process, but also because they will
-    # perform a non-gracefull shutdown if the process takes too long to terminate.
-
-    hint = f" ({reason})" if reason else f""
-    logger.info('Shutting down Trinity%s', hint)
-
-    for process in processes:
-        # Our sub-processes will have received a SIGINT already (see comment above), so here we
-        # wait 2s for them to finish cleanly, and if they fail we kill them for real.
-        process.join(2)
-        if process.is_alive():
-            kill_process_gracefully(process, logger)
-        logger.info('%s process (pid=%d) terminated', process.name, process.pid)
-
-    remove_dangling_ipc_files(logger, trinity_config.ipc_dir)
-
-    argparse.ArgumentParser().exit(message=f"Trinity shutdown complete{hint}\n")
--- trinity/main.py
+++ trinity/main.py
@@ -1,11 +1,3 @@
-from argparse import ArgumentParser
-import logging
-import multiprocessing
-from typing import (
-    Tuple,
-    Type,
-)
-
 from eth.db.backends.level import LevelDB
 from eth.db.chain import ChainDB
 
@@ -19,85 +11,36 @@ from trinity.config import (
 from trinity.constants import (
     APP_IDENTIFIER_ETH1,
 )
-from trinity.db.manager import DBManager
-from trinity.initialization import (
-    is_database_initialized,
-    initialize_database,
-    ensure_eth1_dirs,
-)
 from trinity.components.registry import (
     get_components_for_eth1_client,
 )
-from trinity._utils.ipc import (
-    wait_for_ipc,
-    kill_process_gracefully,
-)
-from trinity._utils.logging import (
-    child_process_logging,
-)
-from trinity._utils.mp import (
-    ctx,
+from trinity.initialization import (
+    ensure_eth1_dirs,
+    initialize_database,
+    is_database_initialized,
 )
 
 
 def main() -> None:
     main_entry(
         trinity_boot,
+        get_base_db,
         APP_IDENTIFIER_ETH1,
         get_components_for_eth1_client(),
         (Eth1AppConfig,)
     )
 
 
-def trinity_boot(boot_info: BootInfo) -> Tuple[multiprocessing.Process]:
+def get_base_db(boot_info: BootInfo) -> LevelDB:
+    app_config = boot_info.trinity_config.get_app_config(Eth1AppConfig)
+    base_db = LevelDB(db_path=app_config.database_dir)
+    chaindb = ChainDB(base_db)
+    if not is_database_initialized(chaindb):
+        chain_config = app_config.get_chain_config()
+        initialize_database(chain_config, chaindb, base_db)
+    return base_db
+
+
+def trinity_boot(boot_info: BootInfo) -> None:
     trinity_config = boot_info.trinity_config
     ensure_eth1_dirs(trinity_config.get_app_config(Eth1AppConfig))
-
-    logger = logging.getLogger('trinity')
-
-    # First initialize the database process.
-    database_server_process: multiprocessing.Process = ctx.Process(
-        name="DB",
-        target=run_database_process,
-        args=(
-            boot_info,
-            LevelDB,
-        ),
-    )
-
-    # start the processes
-    database_server_process.start()
-    logger.info("Started DB server process (pid=%d)", database_server_process.pid)
-
-    # networking process needs the IPC socket file provided by the database process
-    try:
-        wait_for_ipc(trinity_config.database_ipc_path)
-    except TimeoutError:
-        logger.error("Timeout waiting for database to start.  Exiting...")
-        kill_process_gracefully(database_server_process, logger)
-        ArgumentParser().error(message="Timed out waiting for database start")
-        return None
-
-    return (database_server_process,)
-
-
-def run_database_process(boot_info: BootInfo, db_class: Type[LevelDB]) -> None:
-    with child_process_logging(boot_info):
-        trinity_config = boot_info.trinity_config
-
-        with trinity_config.process_id_file('database'):
-            app_config = trinity_config.get_app_config(Eth1AppConfig)
-
-            base_db = db_class(db_path=app_config.database_dir)
-            chaindb = ChainDB(base_db)
-
-            if not is_database_initialized(chaindb):
-                chain_config = app_config.get_chain_config()
-                initialize_database(chain_config, chaindb, base_db)
-
-            manager = DBManager(base_db)
-            with manager.run(trinity_config.database_ipc_path):
-                try:
-                    manager.wait_stopped()
-                except KeyboardInterrupt:
-                    pass
