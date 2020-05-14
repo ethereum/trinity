6e5a03f1aea40e1a0c8c1fd2e0b5fad42b1b6be2
Alex Stokes <r.alex.stokes@gmail.com>
2020-05-14 15:46:15 -0700

    add basic block handling to the beacon node

--- trinity/nodes/beacon/full.py
+++ trinity/nodes/beacon/full.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import Type
+from typing import Set, Type
 
 from eth.db.backends.level import LevelDB
 from eth_keys.datatypes import PrivateKey
@@ -12,8 +12,10 @@ from eth2.api.http.validator import Context
 from eth2.api.http.validator import ServerHandlers as ValidatorAPIHandlers
 from eth2.api.http.validator import SyncerAPI, SyncStatus
 from eth2.beacon.chains.base import BaseBeaconChain
+from eth2.beacon.chains.exceptions import ParentNotFoundError, SlashableBlockError
 from eth2.beacon.db.chain import BeaconChainDB
-from eth2.beacon.typing import Slot
+from eth2.beacon.types.blocks import SignedBeaconBlock
+from eth2.beacon.typing import Root, Slot
 from eth2.clock import Clock, Tick, TimeProvider, get_unix_time
 from eth2.configs import Eth2Config
 from trinity._utils.trio_utils import JSONHTTPServer
@@ -86,6 +88,8 @@ class BeaconNode:
 
         self._chain = chain_class(self._base_db, eth2_config)
 
+        self._block_pool: Set[SignedBeaconBlock] = set()
+
         api_context = Context(
             client_identifier,
             chain_config.genesis_time,
@@ -146,6 +150,48 @@ class BeaconNode:
                 "validator HTTP API server listening on %d", self.validator_api_port
             )
             task_status.started()
+
+    def on_block(self, block: SignedBeaconBlock) -> None:
+        try:
+            self._chain.import_block(block)
+        except ParentNotFoundError as exc:
+            self._on_orphan_block(block, exc)
+        except SlashableBlockError as exc:
+            self._on_slashable_block(block, exc)
+        except Exception as exc:
+            self._on_block_failure(block, exc)
+        else:
+            self._on_block_imported(block)
+
+    def _try_import_orphan(self, imported_parent_root: Root) -> None:
+        for orphan in self._block_pool:
+            if orphan.message.parent_root == imported_parent_root:
+                break
+        self._block_pool.discard(orphan)
+        self.on_block(orphan)
+
+    def _on_block_imported(self, block: SignedBeaconBlock) -> None:
+        self.logger.debug("successfully imported block %s", block)
+        self._try_import_orphan(block.message.hash_tree_root)
+        # TODO: synchronize any operations from pools that are now on-chain
+
+    def _on_block_failure(self, block: SignedBeaconBlock, exc: Exception) -> None:
+        self.logger.exception("failed to import block %s: %s", block, exc)
+        # TODO: do not drop block?
+
+    def _on_orphan_block(
+        self, block: SignedBeaconBlock, exc: ParentNotFoundError
+    ) -> None:
+        self.logger.debug("failed to import block %s: %s", block, exc)
+        self._block_pool.add(block)
+
+    def _on_slashable_block(
+        self, block: SignedBeaconBlock, exc: SlashableBlockError
+    ) -> None:
+        self.logger.warn("failed to import block %s: %s", block, exc)
+        # NOTE: chain will write the block in ``import_block`` but not the block's state
+        # See the place that exception is raised for further rationale.
+        # TODO: pipe to "slasher" software...
 
     async def run(
         self, task_status: TaskStatus[None] = trio.TASK_STATUS_IGNORED
