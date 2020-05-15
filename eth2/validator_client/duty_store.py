c3869e84e7938d156d569af80959169d78c119a6
Alex Stokes <r.alex.stokes@gmail.com>
2020-05-15 16:22:37 -0700

    Simplify duty scheduling
    Prior implementation resulted in duty duplication and scheduling expired duties

--- eth2/validator_client/duty_store.py
+++ eth2/validator_client/duty_store.py
@@ -7,25 +7,41 @@ from eth2.beacon.typing import Slot
 from eth2.clock import Tick
 from eth2.validator_client.duty import Duty
 
+# A particular subdivision of a slot:
+# Tick(t, slot, epoch, count) => (slot, count)
+TickCount = Tuple[Slot, int]
+
+
+def to_tick_count(tick: Tick) -> TickCount:
+    return (tick.slot, tick.count)
+
+
+def _contains_duty_at_tick(duties: Tuple[Duty, ...], duty: Duty) -> bool:
+    """
+    ``duties`` are a collection of duties at one particular tick
+    """
+    return duty.validator_public_key in map(
+        lambda duty: duty.validator_public_key, duties
+    )
+
 
 class DutyStore:
     def __init__(self) -> None:
-        self._store: Dict[Slot, Dict[int, Tuple[Duty, ...]]] = {}
+        self._store: Dict[TickCount, Tuple[Duty, ...]] = defaultdict(tuple)
         self._store_lock = trio.Lock()
 
     async def duties_at_tick(self, tick: Tick) -> Collection[Duty]:
+        target = to_tick_count(tick)
         async with self._store_lock:
-            if tick.slot in self._store:
-                duties_by_tick = self._store[tick.slot]
-                return duties_by_tick.get(tick.count, ())
-            else:
-                return ()
+            return self._store[target]
 
     async def add_duties(self, *duties: Duty) -> None:
         async with self._store_lock:
             for duty in duties:
-                tick = duty.tick_for_execution
-                if tick.slot not in self._store:
-                    self._store[tick.slot] = defaultdict(tuple)
-
-                self._store[tick.slot][tick.count] += (duty,)
+                target = to_tick_count(duty.tick_for_execution)
+                existing_duties = self._store[target]
+                if _contains_duty_at_tick(existing_duties, duty):
+                    continue
+                else:
+                    existing_duties += (duty,)
+                    self._store[target] = existing_duties
