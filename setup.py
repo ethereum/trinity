1f3166018d468b15cb2755c8faaf4fdb0ce80346
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-05-27 10:21:37 +0200

    PR feedback

--- setup.py
+++ setup.py
@@ -198,6 +198,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     # trinity
     entry_points={
