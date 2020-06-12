615d6bcc871ac8c8ae795a39fb55ac6745650cb2
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-06-12 17:28:44 +0200

    Add tests for ropsten and goerli checkpoint resolving

--- tests/integration/test_etherscan_checkpoint_resolver.py
+++ tests/integration/test_etherscan_checkpoint_resolver.py
@@ -5,21 +5,28 @@ from trinity.components.builtin.syncer.cli import (
     parse_checkpoint_uri,
     is_block_hash,
 )
-from trinity.constants import MAINNET_NETWORK_ID
+from trinity.constants import (
+    MAINNET_NETWORK_ID,
+    GOERLI_NETWORK_ID,
+    ROPSTEN_NETWORK_ID,
+)
 
 
-# This is just the score at the tip as it was at some point on August 26th 2019
-# It serves as anchor so that we have *some* minimal expected score to test against.
-MIN_EXPECTED_SCORE = 11631608640717612820968
+# These are just arbitrarily choosen scores that we know can serve as a rough validity check.
+MIN_EXPECTED_MAINNET_SCORE = 11631608640717612820968
+MIN_EXPECTED_GOERLI_SCORE = 4216548
+MIN_EXPECTED_ROPSTEN_SCORE = 30423839501145616
 
 
 @pytest.mark.parametrize(
-    'uri',
+    'uri,network_id,min_expected_score',
     (
-        'eth://block/byetherscan/latest',
+        ('eth://block/byetherscan/latest', MAINNET_NETWORK_ID, MIN_EXPECTED_MAINNET_SCORE),
+        ('eth://block/byetherscan/latest', GOERLI_NETWORK_ID, MIN_EXPECTED_GOERLI_SCORE),
+        ('eth://block/byetherscan/latest', ROPSTEN_NETWORK_ID, MIN_EXPECTED_ROPSTEN_SCORE)
     )
 )
-def test_parse_checkpoint(uri):
-    checkpoint = parse_checkpoint_uri(uri, MAINNET_NETWORK_ID)
-    assert checkpoint.score >= MIN_EXPECTED_SCORE
+def test_parse_checkpoint(uri, network_id, min_expected_score):
+    checkpoint = parse_checkpoint_uri(uri, network_id)
+    assert checkpoint.score >= min_expected_score
     assert is_block_hash(encode_hex(checkpoint.block_hash))
