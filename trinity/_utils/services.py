84bd72d19f8e2591b0d1adfcabf8c5a1589f52ce
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-07-21 14:49:23 +0200

    Implement wait_first for trio and fix run_background_trio_services

--- trinity/_utils/services.py
+++ trinity/_utils/services.py
@@ -1,36 +1,38 @@
 import asyncio
 import contextlib
-from typing import AsyncContextManager, Callable, Sequence
+from typing import Sequence
 
 from async_service import (
     background_asyncio_service,
     background_trio_service,
-    ManagerAPI,
     ServiceAPI,
 )
 
-from p2p.asyncio_utils import wait_first
+from p2p.asyncio_utils import wait_first as wait_first_asyncio
+from trinity._utils.trio_utils import wait_first as wait_first_trio
 
 
 async def run_background_asyncio_services(services: Sequence[ServiceAPI]) -> None:
-    await _run_background_services(services, background_asyncio_service)
-
-
-async def run_background_trio_services(services: Sequence[ServiceAPI]) -> None:
-    await _run_background_services(services, background_trio_service)
-
-
-async def _run_background_services(
-        services: Sequence[ServiceAPI],
-        runner: Callable[[ServiceAPI], AsyncContextManager[ManagerAPI]]
-) -> None:
     async with contextlib.AsyncExitStack() as stack:
         managers = tuple([
-            await stack.enter_async_context(runner(service))
+            await stack.enter_async_context(background_asyncio_service(service))
             for service in services
         ])
         # If any of the services terminate, we do so as well.
-        await wait_first([
+        await wait_first_asyncio([
             asyncio.create_task(manager.wait_finished())
+            for manager in managers
+        ])
+
+
+async def run_background_trio_services(services: Sequence[ServiceAPI]) -> None:
+    async with contextlib.AsyncExitStack() as stack:
+        managers = tuple([
+            await stack.enter_async_context(background_trio_service(service))
+            for service in services
+        ])
+        # If any of the services terminate, we do so as well.
+        await wait_first_trio([
+            manager.wait_finished
             for manager in managers
         ])
