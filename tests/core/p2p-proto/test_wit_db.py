653f264a12e436b6fa7fa623abcd9a157613c5c1
Jason Carver <ut96caarrs@snkmail.com>
2020-11-19 08:50:54 -0800

    Witness hash storage tests, with repeated block
    Repeated persistence with the same block should not consume history
    slots. I tested the test by patching into line 59 of
    trinity/protocol/wit/db.py:
    + if True or block_hash not in recent_blocks_with_witnesses:
    
    The test fails with that change, as expected.
    
    Also, explicitly check that DB eviction works after a block repeat is
    persisted.

--- tests/core/p2p-proto/test_wit_db.py
+++ tests/core/p2p-proto/test_wit_db.py
@@ -48,6 +48,50 @@ async def test_witness_for_recent_blocks():
 
 
 @pytest.mark.asyncio
+async def test_witness_history_on_repeat_blocks():
+    """
+    Repeated blocks should not consume more slots in the limited history of block witnesses
+    """
+    wit_db = AsyncWitnessDB(MemoryDB())
+    hash1 = Hash32Factory()
+    hash1_witnesses = tuple(Hash32Factory.create_batch(5))
+    await wit_db.coro_persist_witness_hashes(hash1, hash1_witnesses)
+
+    hash2 = Hash32Factory()
+    await wit_db.coro_persist_witness_hashes(hash2, tuple(Hash32Factory.create_batch(5)))
+
+    # *almost* push the first witness out of history
+    for _ in range(wit_db._max_witness_history - 2):
+        await wit_db.coro_persist_witness_hashes(Hash32Factory(), Hash32Factory.create_batch(2))
+
+    # It should still be there...
+    loaded_hashes = await wit_db.coro_get_witness_hashes(hash1)
+    assert set(loaded_hashes) == set(hash1_witnesses)
+
+    # Add one more new witness, for an existing block
+    await wit_db.coro_persist_witness_hashes(hash2, Hash32Factory.create_batch(2))
+
+    # That new witness should *not* consume a block slot in history, so the first hash's
+    #   witness should still be available.
+    loaded_hashes = await wit_db.coro_get_witness_hashes(hash1)
+    assert set(loaded_hashes) == set(hash1_witnesses)
+
+
+@pytest.mark.asyncio
+async def test_witness_eviction_on_repeat_blocks():
+    """
+    After witnesses are persisted twice for the same block, make sure that eviction
+    does not cause any failures.
+    """
+    wit_db = AsyncWitnessDB(MemoryDB())
+    hash_ = Hash32Factory()
+    await wit_db.coro_persist_witness_hashes(hash_, Hash32Factory.create_batch(2))
+    await wit_db.coro_persist_witness_hashes(hash_, Hash32Factory.create_batch(2))
+    for _ in range(wit_db._max_witness_history):
+        await wit_db.coro_persist_witness_hashes(Hash32Factory(), Hash32Factory.create_batch(2))
+
+
+@pytest.mark.asyncio
 async def test_witness_union():
     wit_db = AsyncWitnessDB(MemoryDB())
     hash1 = Hash32Factory()
