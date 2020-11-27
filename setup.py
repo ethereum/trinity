3cc4f1771c3b51ead669eb3c2c643b77442c907d
Elnaril <57373038+Elnaril@users.noreply.github.com>
2020-11-27 12:17:54 +0000

    fix argcomplete version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ deps = {
     'trinity': [
         "aiohttp==3.6.0",
         "asks>=2.4.8,<3",
-        "argcomplete>=1.10.0,<2",
+        "argcomplete>=1.12.2,<2",
         "asyncio-run-in-process==0.1.0a10",
         "bloom-filter==1.3",
         "cachetools>=3.1.0,<4.0.0",
